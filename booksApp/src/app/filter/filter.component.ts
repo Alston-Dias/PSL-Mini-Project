f1cc2615c255c103e57531f9fe4571485315923d
reynoldvaz <reynold2.nic0684@gmail.com>
2021-11-16 16:55:44 +0530

    filter-updated

--- booksApp/src/app/filter/filter.component.ts
+++ booksApp/src/app/filter/filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from '../Book';
+import { BooksdataService } from '../booksdata.service';
 
 
 @Component({
@@ -9,9 +12,28 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class FilterComponent implements OnInit {
   @Output() filterBooks = new EventEmitter();
   @Output() allBookEvent = new EventEmitter();
-  constructor() { }
+
+  bookList : Array<Book> =[];
+
+  categories: Array<string>=[]
+
+  uniquecat : Array<string>=[]
+  constructor(private booksdataService: BooksdataService, private router: Router) { }
 
   ngOnInit(): void {
+    this.booksdataService.getBooks().subscribe(
+      (books) => {
+        this.bookList = books;
+        for(var b of this.bookList){
+          console.log(b)
+          this.categories.push(b.category)
+        }
+        this.uniquecat = [...new Set(this.categories)]
+
+        console.log("after uniquecat "+this.uniquecat)
+      },
+      (err) => console.log('Error in fetching data')
+    );
   }
   filterCategory(value:any){
     this.filterBooks.emit(value);
